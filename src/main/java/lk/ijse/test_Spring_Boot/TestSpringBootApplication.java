9e58d9844365412bf58e01d06485a2092b9aa634
Shanilka <shanilka799@gmail.com>
2024-10-13 21:00:04 +0530

    1st commit in this project

---
+++ src/main/java/lk/ijse/test_Spring_Boot/TestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.test_Spring_Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringBootApplication.class, args);
+	}
+
+}
